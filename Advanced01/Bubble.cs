89f4f6f6aa21ef0900d2ffa22c9d38d14d7c7936
madonna-mosaad <madonnamosaad86@gmail.com>
2024-07-25 17:20:23 +0300

    Add project files.

---
+++ Advanced01/Bubble.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced01
+{
+    class Bubble<T> where T : IComparable
+    {
+        public static void Swap(ref T x, ref T y)
+        {
+            T temp = x;
+            x = y;
+            y = temp;
+        }
+        public static void BubbleSort(T[] arr)
+        {
+            if (arr != null)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    for (int j = 0; j < arr.Length - i - 1; j++)
+                    {
+                        if (arr[j].CompareTo(arr[j + 1]) == 1)
+                        {
+                            Swap(ref arr[j], ref arr[j + 1]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Advanced01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Advanced01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Q01
+            //int[] arr = { 9, 2, 3 ,0,8,7,9 };
+            //Bubble<int>.BubbleSort(arr);
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]); 
+            //}
+            #endregion
+
+
+        }
+    }
+}
